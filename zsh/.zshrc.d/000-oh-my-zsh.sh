502022e57c5f656b8bbae7fbeb424561b42a53ec
Austin Ray <austin@austinray.io>
2021-08-04 22:40:19 -0400

    zsh: change to typewritten theme
    Change to the typewritten theme to play around with it.

--- zsh/.zshrc.d/000-oh-my-zsh.sh
+++ zsh/.zshrc.d/000-oh-my-zsh.sh
@@ -8,7 +8,8 @@ export ZSH="$HOME/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="lambda"
+ZSH_THEME="typewritten"
+TYPEWRITTEN_PROMPT_LAYOUT="pure"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
