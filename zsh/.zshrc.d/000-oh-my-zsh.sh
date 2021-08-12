01d8fe4c84f07e616cde3b382b8a0b7a648b32c0
Austin Ray <austin@austinray.io>
2021-08-11 22:13:07 -0400

    zsh: use block as cursor
    Replaced the default underscore with a block. It's easier to visualise
    the insertion position.

--- zsh/.zshrc.d/000-oh-my-zsh.sh
+++ zsh/.zshrc.d/000-oh-my-zsh.sh
@@ -10,6 +10,7 @@ export ZSH="$HOME/.oh-my-zsh"
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
 ZSH_THEME="typewritten"
 TYPEWRITTEN_PROMPT_LAYOUT="pure"
+TYPEWRITTEN_CURSOR="block"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
