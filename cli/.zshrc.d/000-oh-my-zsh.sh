0d3c9f1c96297476744e128bfaaeb04925850734
Austin Ray <austin@austinray.io>
2022-02-15 19:37:41 -0500

    cli(zsh): move typewritten configuration to module
    The Typewritten configuration isn't relevant to oh-my-zsh besides
    specifying the theme.

--- cli/.zshrc.d/000-oh-my-zsh.sh
+++ cli/.zshrc.d/000-oh-my-zsh.sh
@@ -9,8 +9,6 @@ export ZSH="$HOME/.oh-my-zsh"
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
 ZSH_THEME="typewritten"
-TYPEWRITTEN_PROMPT_LAYOUT="pure"
-TYPEWRITTEN_CURSOR="block"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
