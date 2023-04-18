5e64c5b7ba4c4d797aa422c6f9f50e8948f90a24
Austin Ray <austin@austinray.io>
2023-04-17 23:40:04 -0400

    cli(nvim): move plugin configuration into `config/plugins/base.lua`
    This large diff is just copy and pasting the plugin configuration in
    `config/base.lua` into `config/plugins/base.lua` in the respective
    plugin specs. I want to split this out further but this is a step in the
    right direction.
    
    With the plugin configuration in its own file, I've moved lazy.nvim's
    `setup()` call into `init.lua` after setting the global Vim
    configuration so it's easier to see where the plugins are loaded.

--- cli/.config/nvim/init.lua
+++ cli/.config/nvim/init.lua
@@ -1,6 +1,23 @@
 -- ~/.config/nvim/lua/config/base.lua
 require("config.base")
 
+-- Automatically install the `lazy` plugin manager.
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- ~/.config/nvim/lua/config/plugins
+require("lazy").setup("config.plugins")
+
 -- Try loading a work configuration.
 -- ~/.config/nvim/lua/config/work.lua
 pcall(require, "config.work")
